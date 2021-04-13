1b43eaf6a0e2416e7016acf13b1da7d8c5b12c6d
Jakob Rasmussen <jakob1303@hotmail.com>
2021-04-13 14:31:25 +0200

    Moved Assignment 1 projects into folder

---
+++ Hand_in_1/PaxAsteroids/OSGiCommonAsteroids/src/main/java/dk/sdu/mmmi/cbse/common/asteroids/OSGiAsteroid.java
@@ -0,0 +1,11 @@
+package dk.sdu.mmmi.cbse.common.asteroids;
+
+import dk.sdu.mmmi.cbse.common.data.Entity;
+
+/**
+ *
+ * @author corfixen
+ */
+//Refactored so the names match the components (OSGiAsteroid - Asteroid)
+public class OSGiAsteroid extends Entity {
+}
